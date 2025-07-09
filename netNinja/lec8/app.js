36e96e7f867424ef8174280e7ff36de1681a696c
Ardhnant <eyddee@proton.me>
2025-07-09 11:50:57 +0530

    making progress in database

--- netNinja/lec8/app.js
+++ netNinja/lec8/app.js
@@ -1,17 +1,40 @@
 //using legit express to reder html and we will learn about ejs too
 
 const express = require("express");
+const morgan = require("morgan");
+const mongoose = require("mongoose");
 
 //express app
 const app = express();
 
-//listen for requests
-app.listen(6969, () => {
-  console.log("We vibin on 6969 🚀");
-});
+//connecting to mongodb
+const dbUri = "mongodb://localhost:27017/";
+mongoose
+  .connect(dbUri)
+  .then((result) => {
+    console.log("Database is now connected");
+
+    //listen for requests
+    app.listen(3000, () => {
+      console.log("Listening to port 3000...");
+    });
+  })
+  .catch((err) => console.log(err));
 
 //register view engine
 app.set("view engine", "ejs");
+
+app.use((req, res, next) => {
+  console.log("in the next middleware");
+  next();
+});
+
+app.use(morgan("dev"));
+
+app.use((req, res, next) => {
+  res.locals.path = req.path;
+  next();
+});
 
 app.get("/", (req, res) => {
   const blogs = [
